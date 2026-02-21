568f00e04c2620d0e7b96021f63a958cdff7be88
Raffael <rdavila754@gmail.com>
2026-02-21 18:32:01 -0500

    typo

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -11,7 +11,7 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub)date")[:5]
+        return Question.objects.order_by("-pub_date")[:5]
 
 # non-generic view
 def index(request):
