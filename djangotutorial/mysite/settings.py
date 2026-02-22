12a719940450d8ad0c55781c5eb3e98333e907c0
Raffael <rdavila754@gmail.com>
2026-02-22 12:19:01 -0500

    deployable

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#0&j=xsw&_pt2vnk(-s6)i9%$0bt7hb^hz3lnytk+w5p60^t*j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "mysite-env.eba-nhmujxum.us-east-2.elasticbeanstalk.com" ]
 
 
 # Application definition
